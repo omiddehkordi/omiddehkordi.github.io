14b57417f93156a3afbfe7e76e296599e2d5f5f2
omid.dehkordi99@gmail.com <omid@aldora.io>
2026-03-17 15:33:32 -0700

    New Web layout first update

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,20 +5,33 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Engineering Contractor",
+    title: "Part-time Data Researcher",
+    cardImage: "assets/images/experience-page/Acculogic.jpeg",
+    place: "Aldora",
+    time: "(September 2025 – Present) · Remote",
+    desp: "<li>Working on projects to collect and analyze data in the entertainment and gaming industry.</li>",
+  },
+  {
+    title: "Engineering Intern",
+    cardImage: "assets/images/experience-page/Acculogic.jpeg",
+    place: "DigiProces",
+    time: "(May 2025 – June 2025) · Barcelona, Spain",
+    desp: "<li>Developed a dynamic data collection method that responded to technician changes in soldering system programs and collected error information from the technician, enabling the collection of labeled data to train Machine Learning models.</li><li>Led the initiation phase of developing Machine Learning models to predict fixes to soldering system errors in production.</li><li>Collaborated with cross-functional teams to ensure the successful integration of data-driven solutions, enhancing operational efficiency.</li><li>Presented a final presentation on the methods developed to a group of engineers and managers detailing the progress made and next steps.</li>",
+  },
+  {
+    title: "Software and Test Engineer",
     cardImage: "assets/images/experience-page/Acculogic.jpeg",
     place: "Acculogic Inc",
-    time: "(June, 2022 - present)",
-    desp: "<li>Machine Learning: Responsible for researching ML applications on the Flying Probe Robotic Test System.</li> <li>Flying Probe System: Responsible for testing Printed Circuit Boards and Batteries for customers. Ensure orders are finished in a timely fashion and work with customers to provide test data that displayed faults requiring repair</li> <li>Python/C++: Responsible for working on Python/C++ Projects to generate test programs and analyze PCB/Battery test data. Automatic generation of test programs for some projects has cut pre-test time in half</li>",
+    time: "(June 2022 – April 2025) · Lake Forest, California",
+    desp: "<li>Debugged and employed test programs for PCB and battery testing in collaboration with application engineers, reducing the cost of defects for customers by up to 35% in final assembly.</li><li>Created a Python script to generate battery test programs for the FLS Flying Probe Tester, resulting in a 50% reduction in pre-test time — adopted by Lucid and Tesla.</li><li>Developed a program to analyze battery data and output statistics to ensure test validity and improve error detection by 10%.</li><li>Constructed a Python script to convert test coverage data from text to Excel format instantaneously, automating the conversion process.</li><li>Installed probe modules into the FLS tester to increase test coverage and reduce test time by up to 60%.</li><li>Utilized Jupyter Notebook for battery data visualization projects for marketing and research purposes.</li>",
   },
   {
     title: "Computer Science Intern",
     cardImage: "assets/images/experience-page/Acculogic.jpeg",
     place: "Acculogic Inc",
-    time: "(May - Aug, 2021)",
-    desp: "<li>C++ Programming: Responsible for creating C++ programs to evaluate PCB test data produced by tester</li>",
-  }
-  
+    time: "(May 2021 – August 2021) · Lake Forest, California",
+    desp: "<li>Designed a C++ program using Object-oriented principles to evaluate FLS Tester performance by comparing PCB test data.</li>",
+  },
 ];
 
 const showCards2 = () => {
@@ -116,4 +129,3 @@ const showCards3 = () => {
   );
   hackathonsection.innerHTML = output;
 };*/
-document.addEventListener("DOMContentLoaded", showCards3);
